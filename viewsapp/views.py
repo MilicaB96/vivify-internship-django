487c443ea5fb108efe369ed3575732be4c3f8d5b
Milica Baštrica <milicabastrica@Milicas-MacBook-Pro.local>
2022-05-16 13:48:08 +0200

    added HomeView

---
+++ viewsapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+class HomeView(APIView):
+  pass

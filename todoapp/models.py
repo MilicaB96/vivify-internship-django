0791dae1134728e71b882bc41d81eb606a681a62
Milica Baštrica <milicabastrica@Milicas-MacBook-Pro.local>
2022-05-16 11:34:33 +0200

    added todo table

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=255)
+    PRIORITY_CHOICES = [
+        ('CRITICAL','critical'),
+        ('IMPORTANT','important'),
+        ('NORMAL','normal'),
+        ('LOW','low'),
+
+    ]
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default='LOW',
+    )

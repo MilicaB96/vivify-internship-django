599d800ea4d4a175fa523ba64bfda7a623019a62
Milica Baštrica <milicabastrica@Milicas-MacBook-Pro.local>
2022-05-17 14:07:45 +0200

    added admin permission

--- userapp/views.py
+++ userapp/views.py
@@ -1,25 +1,20 @@
-from argparse import Action
-from ast import Return
-from importlib.resources import Resource
-from urllib import response
 from django.shortcuts import render
 from rest_framework.mixins import RetrieveModelMixin
 from django.contrib.auth.models import User
 from userapp.serializer import UserSerializer
-from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializer import MyTokenObtainPairSerializer
 from rest_framework.decorators import action
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
-from rest_framework.permissions import  IsAuthenticated
+from rest_framework.permissions import  IsAuthenticated,IsAdminUser
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet,RetrieveModelMixin):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    @action(detail=False, methods=['get'],url_path='me',url_name='me',
+    permission_classes = [IsAdminUser]
+    @action(detail=False, methods=['get'], url_path='me', url_name='me',
     permission_classes = [IsAuthenticated])
     def get_user_data(self, request):
         user = self.request.user
@@ -28,5 +23,4 @@ class UserViewSet(viewsets.ModelViewSet,RetrieveModelMixin):
 
 class LoginViewSet(TokenObtainPairView):
     serializer_class=MyTokenObtainPairSerializer
-
     

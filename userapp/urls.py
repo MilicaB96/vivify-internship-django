fa6403c10cdb578612248f40799816f852db119c
Milica Baštrica <milicabastrica@Milicas-MacBook-Pro.local>
2022-05-18 08:49:58 +0200

    added templates

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,re_path
 from rest_framework import routers
-from .views import LoginViewSet, UserViewSet 
+from .views import LoginViewSet, UserProfile, UserViewSet 
 
 userRouter = routers.SimpleRouter()
 userRouter.register(r'user', UserViewSet)
 
 userApiView = [
     path('login',LoginViewSet.as_view(), name = 'login'),
+    path('user/<int:user_id>/', UserProfile.as_view(), name ='profile')
 ]
